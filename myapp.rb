4e8bb6ad065252fe2c45f9b31c6a560a82368487
Diego Castillo <djdiego88@gmail.com>
2019-07-09 17:28:32 -0500

    Third commit

--- myapp.rb
+++ myapp.rb
@@ -1,15 +1,22 @@
 require 'sinatra'
 
 #/istriangle?a=1&b=2&c=2
+#/istriangle?a=number&b=number&c=number
 get '/istriangle' do
-    @a = params[:a]
-    @b = params[:b]
-    @c = params[:c]
+    a = params[:a].to_i
+    b = params[:b].to_i
+    c = params[:c].to_i
 
     valor = false
 
-    if(@a > 0 && @b > 0 && @c > 0)
-        valor = true
+    if(a > 0 && b > 0 && c > 0)
+        aplusb = a + b
+        aplusc = a + c
+        bplusc = b + c
+
+        if(aplusb > c && aplusc > b && bplusc > a)
+            valor = true
+        end
     end
     
     "#{valor}"
